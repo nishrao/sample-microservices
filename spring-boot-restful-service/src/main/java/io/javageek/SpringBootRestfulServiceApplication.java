9f24c4586c5b85f241e3d742473eb77ac01c9261
Nishanth Rao <nishanth.rao@sigma-systems.com>
2017-03-02 11:13:28 +0000

    Initial Check in
    Change-Id: I9cb265fec90b759404bd0e2426d2957f996012d3

---
+++ spring-boot-restful-service/src/main/java/io/javageek/SpringBootRestfulServiceApplication.java
@@ -0,0 +1,12 @@
+package io.javageek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulServiceApplication.class, args);
+	}
+}
